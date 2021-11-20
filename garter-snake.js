371783823e67cc290b592c1c30449e7156692952
LukasExists <91223726+lukasexists@users.noreply.github.com>
2021-11-19 21:51:51 -0500

    NEW: SETTINGS THINGS

--- garter-snake.js
+++ garter-snake.js
@@ -1,12 +1,84 @@
-snake.cussize = function() {
-    var cussize = prompt("Enter custom stage size number") // ask for number stuff, will be made better sometime later
-    while (cussize !== parseInt(cussize, 10).toString()) {
-      cussize = prompt("Bruh just enter a damn number");
+var cussize = "56"
+
+snake.gart02 = function() {
+    function selectCus() {
+    var cussize2 = prompt("Enter custom stage size number") // ask for number stuff, will be made better sometime later
+    while (cussize2 !== parseInt(cussize2, 10).toString()) {
+      var cussize2 = prompt("Bruh just enter a damn number");
     }
-    return cussize;
+    return cussize2;
+  };
+  var x = document.createElement("BUTTON");
+  var t = document.createTextNode("Set Custom Stage Size");
+  x.appendChild(t);
+  x.id = "GarterSet1";
+  document.body.appendChild(x);
+    
+  document.getElementById("GarterSet1").addEventListener("click", function() {
+     window.cussize = selectCus();
+      window.snake.gart01();
+  });
 };
 
-snake.garter = function() {
+snake.gart01 = function() {
+  // actual stuff
+  const scripts = document.getElementsByTagName('script');
+  for(let script of scripts) {
+    const req = new XMLHttpRequest();
+    req.open('GET', script.src);
+    req.onload = function() {
+      const code = this.responseText;
+      if(code.indexOf('#A2') === -1)
+        return;
+      
+      // stuff that makes speeds work
+      const limesarebetter = code.match(
+        /[a-zA-Z0-9_$]{1,6}=function\(a\){switch\(a\.[a-zA-Z0-9_$]{1,6}\){case 1:return\.66[^}]*?1}}/
+      )[0];
+      const limename = limesarebetter.match(/[a-zA-Z0-9_$]{1,6}/)[0];
+      const limespeed = limesarebetter.match(/switch\(a\.[a-zA-Z0-9_$]{1,6}\)/)[0].replace('switch(a.', 'this.').replace(')', '');
+      const a = code.match(new RegExp(
+        `this\\.[a-zA-Z0-9_$]{1,6}=[a-zA-Z0-9_$]{1,6}\\*${limename}\\(this\\);`
+      ))[0];
+      var rand = Math.random()
+      eval(
+        limesarebetter.replace(
+          '1.33;',
+          `1.33;case 3:return _soup;case 4:return 2;case 5:return Infinity;case 6:return ${rand};case 7:return 0.1;case 8:return 0.25;`
+        )
+      );
+
+      const soup = code.match(
+        /[a-zA-Z0-9_$]{1,6}\.prototype\.update=function\(\){[^]*?this\)}}}}/
+      )[0];
+      const p = soup.match(
+        /:this\.[a-zA-Z0-9_$]{1,6}\+=1;this\.[a-zA-Z0-9_$]{1,6}\+\+;/
+      )[0];
+      eval('var _soup = 1.33;');
+      eval('var _soep = 1.85;');
+      eval(
+        soup.replace(
+          p,
+          p + `_soup = Math.random() < .5 ? .66 : 1.33;_soep = Math.random() < .5 ? .45 : 1.85;${limespeed} === 3 && (${a.replace(limename + '(this);', '_soup')});${limespeed} === 6 && (${a.replace(limename + '(this);', '_soep')});`
+        )
+      );
+      
+      
+      // stuff that makes stages function
+      eval(
+        code.match(
+          /[a-zA-Z0-9_$]{1,6}=function\(a\){switch\(a\.[a-zA-Z0-9_$]{1,6}\){case 2:return 512;[^]*?256}}/
+        )[0].replace(
+          '96;',
+          `96;case 3:return ${window.cussize};case 4:return 36;case 5:return 1200;case 6:return 3500;case 7:return 10000;case 8:return 25000;case 9:return 100000;`
+        )
+      );
+    };
+    req.send();
+  }  
+};
+
+snake.garter = function() {      
   
   // STAGE SIZES //
   
@@ -65,62 +137,8 @@ snake.garter = function() {
   img.height = 47;
   img.class = 'DqMRee SsAred';
   document.querySelector('#speed').appendChild(img);
-
-  // actual stuff
-  const scripts = document.getElementsByTagName('script');
-  for(let script of scripts) {
-    const req = new XMLHttpRequest();
-    req.open('GET', script.src);
-    req.onload = function() {
-      const code = this.responseText;
-      if(code.indexOf('#A2') === -1)
-        return;
-      
-      // stuff that makes speeds work
-      const limesarebetter = code.match(
-        /[a-zA-Z0-9_$]{1,6}=function\(a\){switch\(a\.[a-zA-Z0-9_$]{1,6}\){case 1:return\.66[^}]*?1}}/
-      )[0];
-      const limename = limesarebetter.match(/[a-zA-Z0-9_$]{1,6}/)[0];
-      const limespeed = limesarebetter.match(/switch\(a\.[a-zA-Z0-9_$]{1,6}\)/)[0].replace('switch(a.', 'this.').replace(')', '');
-      const a = code.match(new RegExp(
-        `this\\.[a-zA-Z0-9_$]{1,6}=[a-zA-Z0-9_$]{1,6}\\*${limename}\\(this\\);`
-      ))[0];
-      var rand = Math.random()
-      eval(
-        limesarebetter.replace(
-          '1.33;',
-          `1.33;case 3:return _soup;case 4:return 2;case 5:return Infinity;case 6:return ${rand};case 7:return 0.1;case 8:return 0.25;`
-        )
-      );
-
-      const soup = code.match(
-        /[a-zA-Z0-9_$]{1,6}\.prototype\.update=function\(\){[^]*?this\)}}}}/
-      )[0];
-      const p = soup.match(
-        /:this\.[a-zA-Z0-9_$]{1,6}\+=1;this\.[a-zA-Z0-9_$]{1,6}\+\+;/
-      )[0];
-      eval('var _soup = 1.33;');
-      eval('var _soep = 1.85;');
-      eval(
-        soup.replace(
-          p,
-          p + `_soup = Math.random() < .5 ? .66 : 1.33;_soep = Math.random() < .5 ? .45 : 1.85;${limespeed} === 3 && (${a.replace(limename + '(this);', '_soup')});${limespeed} === 6 && (${a.replace(limename + '(this);', '_soep')});`
-        )
-      );
-      
-      
-      // stuff that makes stages function
-      eval(
-        code.match(
-          /[a-zA-Z0-9_$]{1,6}=function\(a\){switch\(a\.[a-zA-Z0-9_$]{1,6}\){case 2:return 512;[^]*?256}}/
-        )[0].replace(
-          '96;',
-          `96;case 3:return ${window.snake.cussize()};case 4:return 36;case 5:return 1200;case 6:return 3500;case 7:return 10000;case 8:return 25000;case 9:return 100000;`
-        )
-      );
-    };
-    req.send();
-  }
 };
 
-window.snake.garter()
+window.snake.garter();
+window.snake.gart01();
+window.snake.gart02();
